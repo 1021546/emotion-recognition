5f36eb4c0de80276df6aaeb34336cceb805abe86
1021546 <jerryhuang0312@gmail.com>
2017-08-24 17:28:38 +0800

    Update new1.py

--- new1.py
+++ new1.py
@@ -14,6 +14,7 @@ for filename in os.listdir('.'):
         #print last_line
         last_line = last_line[10:-3].split(",") #remove " 'unknown', " and " ,? " then splited by ","
         #print type(last_line)
+	#print type(last_line[0])
         file_data.append(last_line)	
         loadFile.close()
 		
